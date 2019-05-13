d4023d643b27ab3f4465a791f4728b1f40d1a8bb
dropkickfish <thackrayaf@gmail.com>
2019-05-13 21:30:12 +0100

    cleaned up comments, text files include new scores

--- ex36.rb
+++ ex36.rb
@@ -1,16 +1,3 @@
-# Homework
-# Now write a game similar to the one that I created in the last exercise.
-# It can be any kind of game you want in the same flavor.
-# Spend a week on it making it as interesting as possible.
-# For Study Drills, use arrays, functions, and modules as much as possible,
-# and find as many new pieces of Ruby as you can to make the game work.
-#
-# Before you start coding you must draw a map for your game. Create the rooms,
-# monsters, and traps that the player must go through on paper before you code.
-#
-# Once you have your map, try to code it up. If you find problems with the map
-# then adjust it and make the code match.
-
 # Player stats
 $energy = 9
 $music = 0
