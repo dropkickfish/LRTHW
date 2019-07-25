f7c6ba6198ec14892b4e1e69e9edf353942268b9
dropkickfish <thackrayaf@gmail.com>
2019-07-25 21:31:58 +0100

    Fixed basic collision. Need to re-examine playerpos variable + similar

--- Grid based game/Engine.rb
+++ Grid based game/Engine.rb
@@ -1,7 +1,25 @@
-require_relative 'board'
+require_relative 'grid'
 require_relative 'player'
 
 module Engine
+  def self.collisioncheck
+
+    if $playerx  == $monsterx && $playery  == $monstery
+      puts "Fight!"
+    end
+    if $playerx  == $treasurex && $playery  == $treasurey
+      puts "You picked up some treasure"
+    if $playerx  == $exitx && $playery  == $exity
+      puts "You move to the next floor of the dungeon"
+        end
+    if $monsterx == $treasurex && $monstery == $treasurey
+      puts "The monster stole your treasure!"
+        end
+    if  $monsterx == $exitx && $monstery == $exity
+      puts "The monster blocks the exit!"
+      end
+  end
+
   def next
   end
   def fight
@@ -11,9 +29,14 @@ module Engine
   def death
   end
 end
+end
 
 Grid.new
 Player.moveup
+Engine.collisioncheck
 Player.moveleft
+Engine.collisioncheck
 Player.movedown
+Engine.collisioncheck
 Player.moveright
+Engine.collisioncheck
---
+++ Grid based game/Grid.rb
@@ -0,0 +1,104 @@
+
+$playerx = 0
+$playery = 0
+$playerpos = [[$playery.to_i][$playerx.to_i]]
+
+$monsterx = 0
+$monstery = 0
+$monsterpos = [[$monstery.to_i][$monsterx.to_i]]
+
+$treasurex = 0
+$treasurey = 0
+$treasurepos = [[$treasurey.to_i][$treasurex.to_i]]
+
+$exitx = 0
+$exity = 0
+$exitpos = [[$exity.to_i][$exitx.to_i]]
+
+$grid = [
+  [' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ']
+]
+
+def display_grid
+  puts  %Q(
+      +---+---+---+---+---+
+      |   |   |   |   |   |                @ = Player
+      | #{$grid[0][0]} | #{$grid[0][1]} | #{$grid[0][2]} | #{$grid[0][3]} | #{$grid[0][4]} |                M = Monster
+      |   |   |   |   |   |                £ = Treasure
+      +-------------------+                > = Exit
+      |   |   |   |   |   |
+      | #{$grid[1][0]} | #{$grid[1][1]} | #{$grid[1][2]} | #{$grid[1][3]} | #{$grid[1][4]} |
+      |   |   |   |   |   |
+      +-------------------+
+      |   |   |   |   |   |
+      | #{$grid[2][0]} | #{$grid[2][1]} | #{$grid[2][2]} | #{$grid[2][3]} | #{$grid[2][4]} |
+      |   |   |   |   |   |
+      +-------------------+
+      |   |   |   |   |   |
+      | #{$grid[3][0]} | #{$grid[3][1]} | #{$grid[3][2]} | #{$grid[3][3]} | #{$grid[3][4]} |
+      |   |   |   |   |   |
+      +---+---+---+---+---+
+
+)
+end
+
+def randplayerstart
+  $playerx = rand(5)
+  $playery = rand(4)
+  $grid[$playery.to_i][$playerx.to_i] = '@'
+
+end
+
+def randmonsterstart
+  $monsterx = rand(5)
+  $monstery = rand(4)
+  if $grid[$monstery.to_i][$monsterx.to_i].include? '@'
+    randmonsterstart
+  else
+    $grid[$monstery.to_i][$monsterx.to_i] = 'M'
+end
+end
+
+def randtreasurestart
+  $treasurex = rand(5)
+  $treasurey = rand(4)
+
+  if  $grid[$treasurey.to_i][$treasurex.to_i].include?('@'||'M')
+    randtreasurestart
+
+  else
+  $grid[$treasurey.to_i][$treasurex.to_i] = '£'
+
+  end
+
+end
+
+def randexitstart
+  $exitx = rand(5)
+  $exity = rand(4)
+  if   $grid[$exity.to_i][$exitx.to_i].include?('@'||'M'||'£')
+    randexitstart
+  else
+  $grid[$exity.to_i][$exitx.to_i] = '>'
+end
+end
+
+def startpos
+
+  randplayerstart
+  randmonsterstart
+  randtreasurestart
+  randexitstart
+
+end
+
+class Grid
+
+  def initialize
+    startpos
+    display_grid
+  end
+end
